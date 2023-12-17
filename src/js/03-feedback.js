b48f4d11b0e2c00347b8567655f4858a2ef2170c
Kelly Bahilidad <kelly.workmode.home@gmail.com>
2023-12-17 21:03:50 +0800

    Feedback form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,25 @@
+import throttle from 'lodash.throttle';
 
+const formSubmit = document.querySelector('.feedback-form');
+const value = localStorage.getItem('feedback-form-state');
+if (value !== null) {
+  const parseValue = JSON.parse(value);
+  const { email, message } = formSubmit;
+  email.value = parseValue.email;
+  message.value = parseValue.message;
+}
+
+const formHandler = event => {
+  event.preventDefault();
+  //   console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  localStorage.removeItem('feedback-form-state');
+  event.currentTarget.reset();
+};
+const inputHandler = event => {
+  const { email, message } = event.currentTarget;
+  const formItems = { email: email.value, message: message.value };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formItems));
+};
+
+formSubmit.addEventListener('input', inputHandler);
+formSubmit.addEventListener('submit', formHandler);
